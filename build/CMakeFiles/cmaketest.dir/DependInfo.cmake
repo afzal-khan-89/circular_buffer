70cc418a63e4df54d4ac2647fe3991d7aafd18ee
afzal khan <afzal.hossain@tetonelectronics.com>
2024-02-02 16:45:15 +0600

    added cmake

---
+++ build/CMakeFiles/cmaketest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/afzal-khan/study/c/cirular_buffer/circularbuff.c" "CMakeFiles/cmaketest.dir/circularbuff.c.o" "gcc" "CMakeFiles/cmaketest.dir/circularbuff.c.o.d"
+  "/home/afzal-khan/study/c/cirular_buffer/main.c" "CMakeFiles/cmaketest.dir/main.c.o" "gcc" "CMakeFiles/cmaketest.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
